ce39156978cb443bf0283c6948ee15b12340190a
masayoshi <tomoyasu1drums@gmail.com>
2024-07-21 20:05:21 +0900

    fix: be able to open country.index and JapanesePlayers.index.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,27 @@
 <!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    @vite('resources/css/app.css')
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <title>{{ config("app.name"), 'Laravel' }}</title>
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-</head>
-<body>
-    <header class="navbar navbar-dark bg-dark min-h-10 py-4">
-        <div class="container text-xl mx-auto my-0">
-            <a href="/" class="navbar-brand">{{ config("app.name") }}</a>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <header class="navbar navbar-dark bg-dark min-h-10 py-4">
+            <div class="container text-xl mx-auto my-0">
+                <a href="/" class="navbar-brand">{{ config("app.name") }}</a>
+            </div>
+        </header>
+        <div class="container py-4 mx-auto my-0">
+            @yield("content")
         </div>
-    </header>
-    <div class="container py-4 mx-auto my-0">
-        @yield("content")
-    </div>
-</body>
+    </body>
 </html>
