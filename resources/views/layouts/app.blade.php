3eb22d9b1519ef5522eb3b2d8aea77df3c43a366
masayoshi <tomoyasu1drums@gmail.com>
2024-07-22 23:41:00 +0900

    fixed in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,14 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
     </head>
     <body class="font-sans antialiased">
         <header class="navbar navbar-dark bg-dark min-h-10 py-4">
