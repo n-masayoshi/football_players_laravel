4376fef496cf7ae2f5c8da1a228fcfb74fc704b5
masayoshi <tomoyasu1drums@gmail.com>
2024-07-22 23:00:50 +0900

    country,index to Country.index

--- app/Http/Controllers/CountriesController.php
+++ app/Http/Controllers/CountriesController.php
@@ -14,7 +14,7 @@ class CountriesController extends Controller
     {
         $countries = Country::select('country_name')->paginate(5);
         // return view("country.index", compact('countries'));
-        return view("country.index");
+        return view("Country.index");
     }
 
     /**
