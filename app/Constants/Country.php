4ee445a3763e3b3eb80e3e3ddc3f78251dbd7354
masayoshi <tomoyasu1drums@gmail.com>
2024-07-24 22:06:33 +0900

    add:定数クラス-国ID

---
+++ app/Constants/Country.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Constants;
+
+use Ramsey\Uuid\Type\Integer;
+
+// 各国のID
+class CountriesName
+{
+    const JAPAN = 1;
+    const SPAIN = 2;
+    const BRAZIL = 3;
+    const FRANCE = 4;
+    const NETHERLANDS = 5;
+    const KOREA = 6;
+    const SWITZERLAND = 7;
+    const ENGLAND = 8;
+    const BELGIUM = 9;
+    const ARGENTINA = 10;
+    const PORTUGAL = 11;
+    const CROATIA = 12;
+    const ITALY = 13;
+    const AMERICA = 14;
+    const MOROCCO = 15;
+    const COLOMBIA = 16;
+    const URUGUAY = 17;
+    const MEXICO = 18;
+    const GERMANY = 19;
+    const SENEGAL = 20;
+}
