46c534f6ead8ce2770bf1677ee7c435b46fd211a
masayoshi <tomoyasu1drums@gmail.com>
2024-07-29 22:15:49 +0900

    fix: flash-message design.

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -2,11 +2,11 @@
 
 @php
 // if(session('status') === 'info'){$bgColor = 'bg-blue-300';}
-if(session('status') === 'alert'){$bgColor = 'bg-red-500';}
+if(session('status') === 'alert'){$bgColor = 'bg-red-100';}
 @endphp
 
 @if(session('message'))
-    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 my-4 text-white">
+    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 my-4 border border-red-400 rounded px-4 py-3 text-red-700">
         {{ session('message' )}}
     </div>
 @endif
