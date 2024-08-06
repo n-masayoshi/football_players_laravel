a6214d30801e9bc05c119f70ee0863e91a7fa931
masayoshi <tomoyasu1drums@gmail.com>
2024-08-06 23:37:39 +0900

    Modified App Name.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Football Players</title>
 
         <link href="https://cdn.jsdelivr.net/npm/flowbite@latest/dist/flowbite.min.css" rel="stylesheet">
 
@@ -18,7 +18,7 @@
     <body class="font-sans antialiased">
         <header class="navbar navbar-dark bg-dark min-h-10 py-4 max-w-screen-lg mx-auto">
             <div class="container text-xl mx-auto my-0">
-                <a href="/" class="navbar-brand">{{ config("app.name") }}</a>
+                <a href="/" class="navbar-brand">Football Players</a>
             </div>
         </header>
         <div class="container py-4 mx-auto my-0">
