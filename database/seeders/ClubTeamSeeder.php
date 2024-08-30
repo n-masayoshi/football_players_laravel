a7f99081dcd0271ebc0e93160d093575f2c13169
masayoshi <tomoyasu1drums@gmail.com>
2024-08-30 09:26:18 +0900

    added England players data.
    イングランド人選手のデータを挿入。

--- database/seeders/ClubTeamSeeder.php
+++ database/seeders/ClubTeamSeeder.php
@@ -13,27 +13,27 @@ class ClubTeamSeeder extends Seeder
      */
     public function run(): void
     {
-        ClubTeam::create(['club_team_id' => 1, 'club_team_name' => 'アーセナル', 'country_id' => 8]);
-        ClubTeam::create(['club_team_id' => 2, 'club_team_name' => 'レアル・ソシエダ', 'country_id' => 2]);
-        ClubTeam::create(['club_team_id' => 3, 'club_team_name' => 'ブライトン', 'country_id' => 8]);
-        ClubTeam::create(['club_team_id' => 4, 'club_team_name' => 'リバプール', 'country_id' => 8]);
-        ClubTeam::create(['club_team_id' => 5, 'club_team_name' => 'マンチェスター・シティ', 'country_id' => 8]);
-        ClubTeam::create(['club_team_id' => 6, 'club_team_name' => 'マンチェスター・ユナイテッド', 'country_id' => 8]);
-        ClubTeam::create(['club_team_id' => 7, 'club_team_name' => 'チェルシー', 'country_id' => 8]);
-        ClubTeam::create(['club_team_id' => 8, 'club_team_name' => 'トッテナム', 'country_id' => 8]);
-        ClubTeam::create(['club_team_id' => 9, 'club_team_name' => 'ニューカッスル', 'country_id' => 8]);
-        ClubTeam::create(['club_team_id' => 10, 'club_team_name' => 'レアル・マドリード', 'country_id' => 2]);
-        ClubTeam::create(['club_team_id' => 11, 'club_team_name' => 'バルセロナ', 'country_id' => 2]);
-        ClubTeam::create(['club_team_id' => 12, 'club_team_name' => 'アトレティコ・マドリード', 'country_id' => 2]);
-        ClubTeam::create(['club_team_id' => 13, 'club_team_name' => 'バイエルン', 'country_id' => 19]);
-        ClubTeam::create(['club_team_id' => 14, 'club_team_name' => 'ドルトムント', 'country_id' => 19]);
-        ClubTeam::create(['club_team_id' => 15, 'club_team_name' => 'ボルシアMG', 'country_id' => 19]);
-        ClubTeam::create(['club_team_id' => 16, 'club_team_name' => 'フライブルク', 'country_id' => 19]);
-        ClubTeam::create(['club_team_id' => 17, 'club_team_name' => 'モナコ', 'country_id' => 4]);
-        ClubTeam::create(['club_team_id' => 18, 'club_team_name' => 'スタッド・ランス', 'country_id' => 4]);
-        ClubTeam::create(['club_team_id' => 19, 'club_team_name' => 'パリ・サンジェルマン', 'country_id' => 4]);
-        ClubTeam::create(['club_team_id' => 20, 'club_team_name' => 'インテル', 'country_id' => 13]);
-        ClubTeam::create(['club_team_id' => 21, 'club_team_name' => 'ACミラン', 'country_id' => 13]);
-        ClubTeam::create(['club_team_id' => 22, 'club_team_name' => 'ユベントス', 'country_id' => 13]);
+        // ClubTeam::create(['club_team_id' => 1, 'club_team_name' => 'アーセナル', 'country_id' => 8]);
+        // ClubTeam::create(['club_team_id' => 2, 'club_team_name' => 'レアル・ソシエダ', 'country_id' => 2]);
+        // ClubTeam::create(['club_team_id' => 3, 'club_team_name' => 'ブライトン', 'country_id' => 8]);
+        // ClubTeam::create(['club_team_id' => 4, 'club_team_name' => 'リバプール', 'country_id' => 8]);
+        // ClubTeam::create(['club_team_id' => 5, 'club_team_name' => 'マンチェスター・シティ', 'country_id' => 8]);
+        // ClubTeam::create(['club_team_id' => 6, 'club_team_name' => 'マンチェスター・ユナイテッド', 'country_id' => 8]);
+        // ClubTeam::create(['club_team_id' => 7, 'club_team_name' => 'チェルシー', 'country_id' => 8]);
+        // ClubTeam::create(['club_team_id' => 8, 'club_team_name' => 'トッテナム', 'country_id' => 8]);
+        // ClubTeam::create(['club_team_id' => 9, 'club_team_name' => 'ニューカッスル', 'country_id' => 8]);
+        // ClubTeam::create(['club_team_id' => 10, 'club_team_name' => 'レアル・マドリード', 'country_id' => 2]);
+        // ClubTeam::create(['club_team_id' => 11, 'club_team_name' => 'バルセロナ', 'country_id' => 2]);
+        // ClubTeam::create(['club_team_id' => 12, 'club_team_name' => 'アトレティコ・マドリード', 'country_id' => 2]);
+        // ClubTeam::create(['club_team_id' => 13, 'club_team_name' => 'バイエルン', 'country_id' => 19]);
+        // ClubTeam::create(['club_team_id' => 14, 'club_team_name' => 'ドルトムント', 'country_id' => 19]);
+        // ClubTeam::create(['club_team_id' => 15, 'club_team_name' => 'ボルシアMG', 'country_id' => 19]);
+        // ClubTeam::create(['club_team_id' => 16, 'club_team_name' => 'フライブルク', 'country_id' => 19]);
+        // ClubTeam::create(['club_team_id' => 17, 'club_team_name' => 'モナコ', 'country_id' => 4]);
+        // ClubTeam::create(['club_team_id' => 18, 'club_team_name' => 'スタッド・ランス', 'country_id' => 4]);
+        // ClubTeam::create(['club_team_id' => 19, 'club_team_name' => 'パリ・サンジェルマン', 'country_id' => 4]);
+        // ClubTeam::create(['club_team_id' => 20, 'club_team_name' => 'インテル', 'country_id' => 13]);
+        // ClubTeam::create(['club_team_id' => 21, 'club_team_name' => 'ACミラン', 'country_id' => 13]);
+        // ClubTeam::create(['club_team_id' => 22, 'club_team_name' => 'ユベントス', 'country_id' => 13]);
     }
 }
